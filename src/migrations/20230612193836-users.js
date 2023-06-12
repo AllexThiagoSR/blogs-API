170864a2fc3bc0d739fd1cb4c684a2151972e4f9
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-06-12 16:49:00 -0300

    Requisito 1: Adiciona uma coluna que faltou na tabela users

--- src/migrations/20230612193836-users.js
+++ src/migrations/20230612193836-users.js
@@ -11,8 +11,12 @@ module.exports = {
           autoIncrement: true,
           primaryKey: true,
         },
-        displaName: {
+        displayName: {
           field: 'display_name',
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        email: {
           type: Sequelize.STRING,
           allowNull: false,
         },
