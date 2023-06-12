b7a9349c2d96adc6d39277649fd311260813d5b0
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-06-12 17:59:09 -0300

    Requisito 3: Implementa a rota /login

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
 const express = require('express');
-
-// ...
+const { userRouter } = require('./routes');
+const { userController } = require('./controller');
 
 const app = express();
 
-// não remova ou mova esse endpoint
 app.get('/', (_request, response) => {
   response.send();
 });
 
 app.use(express.json());
 
-// ...
+app.use('/users', userRouter);
 
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
+app.post('/login', userController.login);
+
 module.exports = app;
