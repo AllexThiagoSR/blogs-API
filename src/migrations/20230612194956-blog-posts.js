ff4d0088bb9746d57d971dfcde62b4ed0c6c143f
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-06-12 17:00:38 -0300

    Requisito 1: Cria migration para a tabela blog_posts

---
+++ src/migrations/20230612194956-blog-posts.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+      'blog_posts',
+      { 
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        title: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        content: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          field: 'user_id',
+          references: {
+            model: 'users',
+            key: 'id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+        published: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
+        updated: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+      },
+    );
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
