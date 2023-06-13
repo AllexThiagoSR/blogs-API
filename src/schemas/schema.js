3adb2841c882782746a477f68a55ddc121620719
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-06-13 16:42:47 -0300

    Requisito 12: Inclui a verificação dos ids das categorias

--- src/schemas/schema.js
+++ src/schemas/schema.js
@@ -16,4 +16,10 @@ const nameSchema = Joi.object({
   name: Joi.string().required(),
 });
 
-module.exports = { loginSchema, newUserSchema, nameSchema };
+const postSchema = Joi.object({
+  title: Joi.string().required(),
+  content: Joi.string().required(),
+  categoryIds: Joi.array().required(),
+});
+
+module.exports = { loginSchema, newUserSchema, nameSchema, postSchema };
--- src/services/post.service.js
+++ src/services/post.service.js
@@ -7,24 +7,33 @@ const env = process.env.NODE_ENV || 'development';
 
 const sequelize = new Sequelize(config[env]);
 
-const create = async (userId, { title, content, categoryIds }) => {
-  const result = await sequelize.transaction(async (transaction) => {
-    const post = await BlogPost.create({ title, userId, content }, { transaction });
+const { Op } = Sequelize;
 
-    const postCategories = await Promise.all(categoryIds.map(async (id) => {
-      const category = await Category.findByPk(id);
-      if (!category) return undefined;
-      return { postId: post.id, categoryId: category.id };
-    }));
+const createPostCategories = async (categoryIds, postId) => {
+  const categories = await Category.findAll({ where: { id: { [Op.in]: categoryIds } } });
+  return categories.map(({ id }) => ({ postId, categoryId: id }));
+};
 
-    if (postCategories.includes(undefined)) { 
-      return formatServiceReturn(400, 'one or more "categoryIds" not found');
-    }
-    
-    await PostCategory.bulkCreate(postCategories, { transaction });
-    return formatServiceReturn(201, post);
-  });
-  return result;
+const insertPostWithTransaction = async (userId, { title, content, categoryIds }, transaction) => {
+  const post = await BlogPost.create({ title, userId, content }, { transaction });
+  const postCategories = await createPostCategories(categoryIds, post.id);
+  if (postCategories.length !== categoryIds.length) { 
+    return formatServiceReturn(400, 'one or more "categoryIds" not found');
+  }
+  await PostCategory.bulkCreate(postCategories, { transaction });
+  return formatServiceReturn(201, post);
+};
+
+const create = async (userId, postInfos) => {
+  try {
+    const result = await sequelize.transaction(
+      async (transaction) => insertPostWithTransaction(userId, postInfos, transaction),
+    );
+    return result;
+  } catch (error) {
+    console.log(error);
+    return formatServiceReturn(500, 'Internal sever error');
+  }
 };
 
 module.exports = { create };
